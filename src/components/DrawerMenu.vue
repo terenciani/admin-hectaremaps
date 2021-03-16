e2e831fe54dfa065c11878930725caa176368225
Marcelo Terenciani <marcelo.terenciani@gmail.com>
2021-03-16 11:56:15 -0300

    Drawer Menu

--- src/components/DrawerMenu.vue
+++ src/components/DrawerMenu.vue
@@ -1,41 +1,104 @@
 <template>
     <div>
-        <v-navigation-drawer
-            app
-            v-model="drawer"
-            clipped
-            disable-resize-watcher
-        >
-            <v-list>
-                <template v-for="(item, index) in items">
-                    <v-list-item link :key="index" :to="item.to">
-                        <v-list-item-title class="text-capitalize">
-                            {{ item.text }}
-                        </v-list-item-title>
-                    </v-list-item>
-                    <v-divider :key="`divider-${index}`"></v-divider>
-                </template>
-                <v-list-item link to="/admin">
-                    <v-list-item-title class="text-capitalize">
-                        Entrar
-                    </v-list-item-title>
-                </v-list-item>
-                <v-divider></v-divider>
-                <v-list-item link @click.stop="dialog = true">
-                    <v-list-item-title class="text-capitalize">
-                        Solicitar Cadastro
-                    </v-list-item-title>
-                </v-list-item>
+        <v-app-bar color="teal accent-4" dark app>
+            <v-app-bar-nav-icon
+                @click.stop="drawer = !drawer"
+            ></v-app-bar-nav-icon>
+
+            <v-toolbar-title>{{ toolbarTitle }}</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            <v-list color="transparent">
+                <v-menu offset-y transition="scale-transition">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-list-item v-on="on" v-bind="attrs">
+                            <v-list-item-icon>
+                                <v-icon large>mdi-logout</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                <v-list-item-title class="title">
+                                    {{ user.name }}
+                                </v-list-item-title>
+                                <v-list-item-subtitle>
+                                    {{ user.email }}
+                                </v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </template>
+                    <v-list dense>
+                        <v-list-item @click="logout">
+                            <v-list-item-icon>
+                                <v-icon>mdi-logout</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title>Sair</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
             </v-list>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" temporary absolute>
+            <v-list class="text-xs-center" dense>
+                <v-img
+                    :src="require('@/assets/logo.png')"
+                    style="width:50%; margin: 0 auto;"
+                    alt="Logo"
+                />
+                <v-divider />
+                <v-list-item-group
+                    v-model="group"
+                    v-if="items.length > 0"
+                    active-class="deep-purple--text text--accent-4"
+                >
+                    <template v-for="item in items">
+                        <v-list-item
+                            :key="item.text"
+                            :to="item.to"
+                            @click="toolbarTitle = item.text"
+                        >
+                            <v-list-item-icon>
+                                <v-icon>{{ item.icon }}</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title>{{
+                                item.text
+                            }}</v-list-item-title>
+                        </v-list-item>
+                    </template>
+                </v-list-item-group>
+            </v-list>
+            <template v-slot:append>
+                <div class="pa-2">
+                    <v-menu offset-y transition="scale-transition">
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn block outlined v-bind="attrs" v-on="on">
+                                <v-icon left dark>
+                                    mdi-logout
+                                </v-icon>
+                                Sair
+                            </v-btn>
+                        </template>
+                        <v-list dense>
+                            <v-list-item @click="logout">
+                                <v-list-item-icon>
+                                    <v-icon>mdi-logout</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-title>Sair</v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                </div>
+            </template>
         </v-navigation-drawer>
     </div>
 </template>
 <script>
+import DrawerItems from '../data/DrawerItems';
 import AuthService from '../service/AuthService';
 export default {
     data() {
         return {
-            drawer: false,
+            toolbarTitle: '',
+            user: {},
+            drawer: true,
             group: null,
             items: [
                 {
@@ -51,8 +114,11 @@ export default {
             AuthService.removeUserFromLocalStorage();
             this.$router.push('/');
             this.$store.commit('loggoutUser');
-            this.$emit('loggout');
         }
+    },
+    async mounted() {
+        this.user = AuthService.getLoggedUser();
+        this.items = await DrawerItems.getItems();
     }
 };
 </script>
