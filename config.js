7c67ad584b19d9a858d11b242cf1119bd32f05c7
Marcelo Terenciani <marcelo.terenciani@gmail.com>
2021-03-20 20:11:53 -0300

    correção no arquivo de configuração

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
-    //  apiHost: 'https://hectaremaps-api.herokuapp.com',
-    apiHost: 'http://localhost:3000',
+    apiHost: 'https://hectaremaps-api.herokuapp.com',
+    // apiHost: 'http://localhost:3000',
     publicArea: 'https://hectaremaps-front.herokuapp.com',
     decimalPlaces: 2
 };
